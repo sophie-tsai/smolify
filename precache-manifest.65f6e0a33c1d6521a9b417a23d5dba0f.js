1ee64d51de35aa5686b59ec5378c12879c23fa85
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-06-15 13:39:34 -0700

    Updates

---
+++ precache-manifest.65f6e0a33c1d6521a9b417a23d5dba0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7975d8cbd2c1b57d3cd47ee0e3f8601a",
+    "url": "/smolify/index.html"
+  },
+  {
+    "revision": "468a1eea3bc097e3b33b",
+    "url": "/smolify/static/css/main.e9d7c470.chunk.css"
+  },
+  {
+    "revision": "71526b102106172cab0f",
+    "url": "/smolify/static/js/2.a7ff7c54.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/smolify/static/js/2.a7ff7c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468a1eea3bc097e3b33b",
+    "url": "/smolify/static/js/main.06e5d17e.chunk.js"
+  },
+  {
+    "revision": "2d29601738f35028bd1f",
+    "url": "/smolify/static/js/runtime-main.49e53e57.js"
+  },
+  {
+    "revision": "33545ec6ffda90e786dea627ce363688",
+    "url": "/smolify/static/media/memphis-mini.33545ec6.png"
+  }
+]);
